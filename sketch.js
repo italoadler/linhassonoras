76e12be4c153d986c5397b464c26bcce147ab501
Italo Adler <adler.italo@gmail.com>
2018-05-19 07:48:23 -0300

    melhoramento da interface e síntese

--- sketch.js
+++ sketch.js
@@ -1,17 +1,23 @@
 /*
   @italoadler 2018
   @piece: linhas sonoras
+
 */
 
 let posx, posy;
 
 var attackLevel = 1.0;
-var releaseLevel = 0  ;
+var releaseLevel = 0;
 
-var attackTime = 0.008
-var decayTime = 2.9;
-var susPercent = 0.2;
-var releaseTime = 1.8;
+var attackTime = 0.8;
+var decayTime = 0.5;
+var susPercent = 0.9  ;
+var releaseTime = 1.2;
+var overdub = [[]];
+
+var mic, recorder,soundFile;
+var state = 0;
+
 
 var env, triOsc;
 
@@ -19,11 +25,15 @@ var mod = false;
 var mod2 = false;
 var mod3 = false;
 
+
+
+
+
 function setup() {
   var cnv =   createCanvas(displayWidth, displayHeight);
-  background(0);
+  background(255);
   stroke(255);
-  noCursor();
+
 
   strokeWeight(0.8);
   env = new p5.Env();
@@ -42,21 +52,74 @@ function setup() {
   triOsc3.amp(env);
   triOsc3.start();
 
-
   cnv.mousePressed(playEnv);
+
+  // mic = new p5.AudioIn();
+  //
+  //
+  // mic.start();
+  // recorder = new p5.SoundRecorder();
+  // recorder.setInput(mic);
+  // soundFile = new p5.SoundFile();
 }
 
 function draw() {
-  if(mouseIsPressed) {
-    line(mouseX,mouseY,pmouseX,pmouseY);
-    triOsc.freq(map(mouseY,0,height, 110,800));
-    triOsc2.freq(map(mouseX,0,width,20,5000));
-    triOsc2.freq(map(dist(mouseX,width/2,0,1,100,400)));
-  }
+
 }
 function playEnv(){
-  env.play();
+ env.play();
 }
 
+
+function mousePressed() {
+  firstX = mouseX;
+}
+
+function mouseDragged() {
+  var i,j = 0;
+  var green = map(mouseX, 0, width, 50, 255);
+  var blue = map(mouseY, 0, width, 0, 255);
+  var red = dist(mouseX,mouseY,width/2,height/2);
+  var speed = dist(pmouseX, pmouseY, mouseX, mouseY);
+  var alpha = map(speed, 0, 20, 0, 10);
+  var lineWidth = map(speed, 0, 10, 10, 1);
+  lineWidth = constrain(lineWidth, 1.2, 10);
+
+  noStroke();
+  fill(0, alpha);
+  rect(0, 0, width, height);
+
+  stroke(red, green, blue, 255);
+  strokeWeight(lineWidth);
+  //line(mouseX,mouseY,pmouseX,pmouseY);
+  lineS(mouseX,mouseY,pmouseX,pmouseY,lineWidth);
+  triOsc.freq(map(mouseY,0,height,80,3000));
+  triOsc2.freq(map(mouseX,0,width,80,440));
+  overdub[i] = mouseX;
+  overdub[j] = mouseY;
+  i++;
+  j++;
+}
+
+function mouseReleased() {
+  triOsc3.freq(dist(mouseX,firstX,))
+  for(var i = 0; i < overdub.length; i++) {
+    triOsc3.freq(map(i,0,width,80,110))
+  }
+}
+
+
+function lineS(x,y,px,py,lineWidth) {
+  strokeWeight(lineWidth);
+  line(px,py,x,y);
+  line(px,py,x,y);
+  line(width/2+((width/2)-px),py,width/2+((width/2)-x),y);
+  line(px,height/2+((height/2)-py),x,height/2+((height/2)-y));
+  line(width/2+((width/2)-px),height/2+((height/2)-py),width/2+((width/2)-x),height/2+((height/2)-y));
+  return;
+}
+
+
 function keyPressed() {
+
 }
